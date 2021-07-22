e874853e6c027bc886c950fd680e74c1e0f48eed
BaibhavVishal123 <baibhav.vishal123@gmail.com>
2021-07-22 23:16:58 +0530

    fixing ver tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,14 @@
 variable "APP_NAME" {
   type = string
+  default = "Hello World"
 }
 
 variable "location" {
   type = string
+  default = "westus"
 }
 
 variable "environment" {
   type = string
+  default = "dev"
 }
