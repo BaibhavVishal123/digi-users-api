bb236560f7c9370a7d87ff1b2cde68c499785b98
BaibhavVishal123 <baibhav.vishal123@gmail.com>
2021-07-22 20:37:07 +0530

    cleaning  up

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,36 +6,21 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=2.46.0"
     }
+    backend "azurerm" {
+        resource_group_name  = "TF-ResourceGroup"
+        storage_account_name = "tfsabob"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
   }
 }
 provider "azurerm" {
   features {}
-  subscription_id = "4c8aab75-8197-48e7-a45a-da738a7060ed"
-  client_id       = "9addaa5c-8e37-4b9f-a86d-686e7eb3279c"
-  client_secret   = "CAa_EaTfoM99Ig~pZIrs57NG1UqQgR.UDf"
-  tenant_id       = "6169c379-c30b-43d0-91b8-30ea4874d9a4"
+  subscription_id = "<subscription_id>"
+  client_id       = "<client_id>"
+  client_secret   = "<client_secret>"
+  tenant_id       = "<tenant_id>"
 }
-
-terraform{
-    backend "azurerm" {
-    resource_group_name  = "TF-ResourceGroup"
-    storage_account_name = "tfsabob"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
-
-# Configure the Microsoft Azure Provider
-# {
-# # appId client_id
-#   "appId": "9addaa5c-8e37-4b9f-a86d-686e7eb3279c",
-#   "displayName": "azure-cli-2021-07-22-13-36-05",
-#   "name": "9addaa5c-8e37-4b9f-a86d-686e7eb3279c",
-#   #password client_secret
-#   "password": "CAa_EaTfoM99Ig~pZIrs57NG1UqQgR.UDf",
-#   #tenant tenant_id
-#   "tenant": "6169c379-c30b-43d0-91b8-30ea4874d9a4"
-# }
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
